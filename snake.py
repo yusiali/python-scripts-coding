a94f59ee9b5872b0e212d7cd2dd5dbd3f70b8044
Yusuf Ali <70917481+yusiali@users.noreply.github.com>
2024-07-24 13:01:30 -0700

    Update snake.py
    added scoreboard

--- snake.py
+++ snake.py
@@ -1,44 +1,81 @@
-from turtle import *
-from random import randrange
+"""Snake, classic arcade game.
+
+Exercises
+
+1. How do you make the snake faster or slower?
+2. How can you make the snake go around the edges?
+3. How would you move the food?
+4. Change the snake to respond to mouse clicks.
+"""
+
+from random import randrange, choice
+from turtle import * 
+
 from freegames import square, vector
-food = vector(0,0)
-snake = vector(10,0)
+
+state = {'score': 0}
+writer = Turtle(visible=False)
+food = vector(0, 0)
+snake = [vector(10, 0)]
 aim = vector(0, -10)
-def change(x,y):
+
+def change(x, y):
+    """Change snake direction."""
     aim.x = x
     aim.y = y
 
+
 def inside(head):
-    return -200 < head.x <200 and -200 < head.y < 200
+    """Return True if head inside boundaries."""
+    return -200 < head.x < 190 and -200 < head.y < 190
+
 
 def move():
-    head = snake(-1).copy
+    """Move snake forward one segment."""
+    writer.undo()
+    writer.write(state['score'])
+    clear()
+    head = snake[-1].copy()
+    head.move(aim)
+
     if not inside(head) or head in snake:
-        square(head.x, head.y, 10, 'red')
+        square(head.x, head.y, 9, 'red')
         update()
-        snake.append(head)
+        return
+
+    snake.append(head)
+
     if head == food:
-        print(len(snake))
-        food.x = randrange(-150, 150)
-        food.y = randrange(-150, 150)
+        print('Snake:', len(snake))
+        state['score'] += 1
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
     else:
-        snake.pop(0)   
+        snake.pop(0)
+
+    clear()
+
     for body in snake:
-        square(body.x, body.y, 10, 'black')
-        
-    square(head.x, head.y, 10, 'green')
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
-    
+
 
 setup(420, 420, 370, 0)
+bgcolor('white')
 hideturtle()
 tracer(False)
+writer.penup()
+writer.goto(160, 160)
+writer.color('black')
+writer.write("Score:" , state['score'])
+writer.
 listen()
-onkey(lambda : change(10,0), 'Right')
-onkey(lambda : change(-10,0), 'Left')
-onkey(lambda : change(0,10), 'Up')
-onkey(lambda : change(0,-10), 'Down')
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
 move()
 done()
-
